8ad1375f34b72ab1f2dadd9b70cc5c8584ab14b1
unknown <kalyanar@KALYANAR-W7-4.corp.adobe.com>
2015-08-13 22:29:10 +0530

    fixed

--- packagegenerator/src/main/java/com/adobe/ams/kalyanar/stats/userstats/impl/UserLoginLogoutEventHandler.java
+++ packagegenerator/src/main/java/com/adobe/ams/kalyanar/stats/userstats/impl/UserLoginLogoutEventHandler.java
@@ -5,7 +5,6 @@ import com.adobe.ams.kalyanar.stats.userstats.ResetUserStats;
 import com.google.common.collect.Sets;
 import org.apache.felix.scr.annotations.*;
 import org.apache.sling.api.SlingConstants;
-import org.apache.sling.api.resource.*;
 import org.osgi.framework.Constants;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
@@ -26,47 +25,60 @@ import java.util.concurrent.ConcurrentHashMap;
         @Property(name = Constants.SERVICE_DESCRIPTION, value = "User login logout handler."),
         @Property(name = EventConstants.EVENT_TOPIC, value = {
                 SlingConstants.TOPIC_RESOURCE_ADDED,
-                SlingConstants.TOPIC_RESOURCE_REMOVED } , propertyPrivate = true),
-        @Property(name = EventConstants.EVENT_FILTER, value = "(resourceType=rep:Token)")})
+                SlingConstants.TOPIC_RESOURCE_REMOVED } , propertyPrivate = true)})
 public class UserLoginLogoutEventHandler implements EventHandler,ResetUserStats {
 
     private static Logger logger = LoggerFactory.getLogger(UserLoginLogoutEventHandler.class);
     Map<String,Set<String>> users = new ConcurrentHashMap<String, Set<String>>();
 
     @Reference
-    private ResourceResolverFactory resolverFactory;
-
-    @Reference
     private ConcurrentUserStats concurrentUserStats;
 
     public void handleEvent(Event event) {
         String tokenResourcePath = (String) event.getProperty("path");
-        try {
-            String eventTopic  = event.getTopic();
-            //must change to service user
-            ResourceResolver resourceResolver = resolverFactory.getAdministrativeResourceResolver(null);
-            Resource tokenResource = resourceResolver.getResource(tokenResourcePath);
-            String tokenName = tokenResource.getName();
-            Resource userResource = tokenResource.getParent().getParent();
-            String authorizableId = userResource.adaptTo(ValueMap.class).get("rep:authorizableId","");
-            if(SlingConstants.TOPIC_RESOURCE_ADDED.equals(eventTopic)){
-                Set<String> tokens = getTokens(authorizableId);
-                tokens.add(tokenName);
-                if(!users.containsKey(authorizableId)){
-                    concurrentUserStats.incrementSecByOne();
-                }
-                users.put(authorizableId,tokens);
-            }else if(SlingConstants.TOPIC_RESOURCE_REMOVED.equals(eventTopic)){
-                Set<String> tokens = getTokens(authorizableId);
-                tokens.remove(tokenName);
-                if(tokens.size()==0){
-                    users.remove(authorizableId);
-                    concurrentUserStats.decrementSecByOne();
+
+            if(tokenResourcePath.startsWith("/home/users/")){
+                String eventTopic  = event.getTopic();
+                String tokenName = getTokenName(tokenResourcePath);
+                String authorizableId =getUserName(tokenResourcePath);
+                if("rep:Token".equals(event.getProperty("resourceType"))){
+                    if(SlingConstants.TOPIC_RESOURCE_ADDED.equals(eventTopic)){
+                        Set<String> tokens = getTokens(authorizableId);
+                        tokens.add(tokenName);
+                        if(!users.containsKey(authorizableId)){
+                            concurrentUserStats.incrementSecByOne();
+                        }
+                        users.put(authorizableId, tokens);
+                    }
+                }else{
+                    if(SlingConstants.TOPIC_RESOURCE_REMOVED.equals(eventTopic)){
+                        Set<String> tokens = getTokens(authorizableId);
+                        tokens.remove(tokenName);
+                        if(tokens.size()==0){
+                            users.remove(authorizableId);
+                            concurrentUserStats.decrementSecByOne();
+                        }
+                    }
                 }
             }
-        } catch (LoginException e) {
 
+
+    }
+    private String getTokenName(String token){
+        int startindex = token.lastIndexOf("/");
+        if(startindex!=-1){
+            return token.substring(startindex+1);
         }
+        return token;
+    }
+    private String getUserName(String token){
+        String[] tokens = token.split("/");
+        if(tokens!=null){
+            if(tokens.length-3>-1){
+                return   tokens[tokens.length-3];
+            }
+        }
+        return "";
     }
     private Set<String> getTokens(String authorizableid){
         if(users.containsKey(authorizableid)){

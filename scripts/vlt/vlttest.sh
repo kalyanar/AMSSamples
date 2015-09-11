7641343c68fb1e420bac9ff285fe834d5587c168
kalyanar <gopalkris@gmail.com>
2015-09-11 10:23:10 +0530

    Update vlttest.sh

--- scripts/vlt/vlttest.sh
+++ scripts/vlt/vlttest.sh
@@ -2,16 +2,19 @@
 sourceserverip=localhost
 sourceserverport=4502
 sourceuser=admin
-sourcepwd=admin
-targetserverip=localhost
-targetserverport=4503
+sourcepwd="(%40APr3P0d%23)"
+targetserverip=52.69.196.150
+targetserverport=4502
 targetuser=admin
-targetpwd=admin
-. ./rcp.properties
-sourcepaths=($(cat ./rcp.properties| sed -n -e '/^sourcecontentpath/p'))
-targetpaths=($(cat ./rcp.properties| sed -n -e '/^targetcontentpath/p'))
+#targetpwd=y~oHo<-1y71C
+#urlencoded password
+targetauth="admin:y~oHo%3C-1y71C"
+sourcepaths=($(cat ./rcp.properties|sed -n -e '/^sourcecontentpath/p'))
+targetpaths=($(cat rcp.properties| sed -n -e '/^targetcontentpath/p'))
 today=$(date +'%d-%m-%Y')
 logfilepath=logs/import_${today}.log
+export PATH=$PATH:/home/kalyanar/vault-cli-3.1.16/bin
+#echo $logfilepath
 echo "started at: $(date)" > $logfilepath
 pidarr=()
 for i in "${!sourcepaths[@]}"
@@ -20,7 +23,7 @@ do
    sourcepath=($(echo ${sourcepaths[$i]} | tr "=" "\n") )
    targetpath=($(echo ${targetpaths[$i]} | tr "=" "\n") )
    source="http://$sourceuser:$sourcepwd@$sourceserverip:$sourceserverport/crx/-/jcr:root${sourcepath[1]}"
-target="http://$targetuser:$targetpwd@$targetserverip:$targetserverport/crx/-/jcr:root${targetpath[1]}"
+target="http://$targetauth@$targetserverip:$targetserverport/crx/-/jcr:root${targetpath[1]}"
 
  vlt rcp -r -b 100 -n $source $target  |tee -a $logfilepath >> $logfilepath & 
  
